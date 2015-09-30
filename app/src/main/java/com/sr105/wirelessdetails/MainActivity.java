9687edb55106400e2a886d026be2c1b862cdffc0
Harvey Chapman <hchapman@3gfp.com>
2015-09-30 00:05:11 -0400

    Add an NTP example as well

--- app/src/main/java/com/sr105/wirelessdetails/MainActivity.java
+++ app/src/main/java/com/sr105/wirelessdetails/MainActivity.java
@@ -1,30 +1,63 @@
 package com.sr105.wirelessdetails;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.sr105.ntp.SntpClient;
 import com.sr105.wifi.WifiConfigurationWrapper;
 import com.sr105.wifi.WifiUtils;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+
+    private boolean mDoNtpExample = true;
+    private boolean mDoWifiExample = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.textView);
-        String text = "Build Time: " + new ExtraAppInfo(this).buildTime().toString() + "\n";
+        final String text = "Build Time: " + new ExtraAppInfo(this).buildTime().toString() + "\n";
+        mTextView.setText(text);
+        if (mDoNtpExample)
+            addNtpTimeToTextView();
+        else
+            addWifiInfoToTextView();
+    }
+
+    public void addNtpTimeToTextView() {
+        AsyncTask<String, Void, Date> task = new AsyncTask<String, Void, Date>() {
+            @Override
+            protected Date doInBackground(String[] params) {
+                return SntpClient.getTime(params[0], 20000);
+            }
+
+            @Override
+            protected void onPostExecute(Date date) {
+                String text = mTextView.getText().toString();
+                mTextView.setText(text + "NTP Time: " + date + "\n");
+                addWifiInfoToTextView();
+            }
+        };
+        task.execute("2.android.pool.ntp.org");
+    }
+
+    public void addWifiInfoToTextView() {
+        if (!mDoWifiExample)
+            return;
+        String text = mTextView.getText().toString();
         WifiUtils.sContext = this;
         WifiConfigurationWrapper wifi = WifiUtils.getCurrentWifiConfig();
-        if (wifi != null) {
-            text += "\n" + WifiUtils.logWifi(wifi, "Current");
-        }
-        mTextView.setText(text);
+        String wifiText = "\n" + WifiUtils.logWifi(wifi, "Current");
+        mTextView.setText(text + wifiText);
     }
 
     @Override
---
+++ ntp/src/main/java/com/sr105/ntp/SntpClient.java
@@ -0,0 +1,80 @@
+package com.sr105.ntp;
+
+import android.os.SystemClock;
+
+import org.rdm.util.ReflectionObject;
+
+import java.util.Date;
+
+/**
+ * SntpClient is a proxy (using reflection) of Android's hidden
+ * android.net.SntpClient class.
+ */
+public class SntpClient extends ReflectionObject
+{
+    private static final String className = "android.net.SntpClient";
+
+    public SntpClient() throws ClassNotFoundException
+    {
+        super(className);
+    }
+
+    /**
+     * Sends an SNTP request to the given host and processes the response.
+     *
+     * Note: This is added for convenience. It does not exist in the real SntpClient.
+     *
+     * @param host    host name of the server.
+     * @param timeout network timeout in milliseconds.
+     * @return current time as a Date
+     */
+    public static Date getTime(String host, int timeout)
+    {
+        try
+        {
+            SntpClient client = new SntpClient();
+            if (client.requestTime(host, timeout))
+            {
+                long now = client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
+                return new Date(now);
+            }
+        }
+        catch (Exception ignored)
+        {
+        }
+        return null;
+    }
+
+    /**
+     * Sends an SNTP request to the given host and processes the response.
+     *
+     * @param host    host name of the server.
+     * @param timeout network timeout in milliseconds.
+     * @return true if the transaction was successful.
+     */
+    public boolean requestTime(String host, int timeout)
+    {
+        return invokeBooleanMethod("requestTime", host, timeout);
+    }
+
+    /**
+     * Returns the time computed from the NTP transaction.
+     *
+     * @return time value computed from NTP server response.
+     */
+    public long getNtpTime()
+    {
+        return invokeLongMethod("getNtpTime");
+    }
+
+    /**
+     * Returns the reference clock value (value of SystemClock.elapsedRealtime())
+     * corresponding to the NTP time.
+     *
+     * @return reference clock corresponding to the NTP time.
+     */
+    public long getNtpTimeReference()
+    {
+        return invokeLongMethod("getNtpTimeReference");
+    }
+}
